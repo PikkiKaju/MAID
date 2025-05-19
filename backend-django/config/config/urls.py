ddd3e531361c328bbc0611446e839eb0842780fe
Martyna Kramarz <mk307890@student.polsl.pl>
2025-05-19 20:16:26 +0200

    Update urls.py

--- backend-django/config/config/urls.py
+++ backend-django/config/config/urls.py
@@ -1,22 +1,20 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from .views import (
+    LinearRegressionView,
+    PolynomialRegressionView,
+    RidgeRegressionView,
+    SVRRegressionView,
+    DecisionTreeRegressionView,
+    RandomForestRegressionView,
+    GradientBoostingRegressionView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('linear/', LinearRegressionView.as_view(), name='linear_regression'),
+    path('polynomial/', PolynomialRegressionView.as_view(), name='polynomial_regression'),
+    path('ridge/', RidgeRegressionView.as_view(), name='ridge_regression'),
+    path('svr/', SVRRegressionView.as_view(), name='svr_regression'),
+    path('decision-tree/', DecisionTreeRegressionView.as_view(), name='decision_tree_regression'),
+    path('random-forest/', RandomForestRegressionView.as_view(), name='random_forest_regression'),
+    path('gradient-boosting/', GradientBoostingRegressionView.as_view(), name='gradient_boosting_regression'),
 ]
