e527e2dd1c0f819e7118d7e77d8ee0817ddaa9ab
PikkiKaju <igor.zmuda02@gmail.com>
2025-10-23 00:11:41 +0200

    Add DRF Spectacular for API schema generation and documentation

--- backend-django/config/urls.py
+++ backend-django/config/urls.py
@@ -3,4 +3,7 @@ from django.urls import include, path
 urlpatterns = [
     path("api/", include("regressor.urls")),
     path("api/network/", include("network.urls")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
