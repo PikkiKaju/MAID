8b895611936117dc035952ce47ebfe558b464b93
PikkiKaju <igor.zmuda02@gmail.com>
2025-11-06 06:18:21 +0100

    Update PostgreSQL connection string and adjust Docker configurations for backend services

--- backend-django/config/settings.py
+++ backend-django/config/settings.py
@@ -40,21 +40,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-INSTALLED_APPS += ['corsheaders', 'rest_framework', 'network', 'drf_spectacular', 'rest_framework.authtoken']
+INSTALLED_APPS += ['corsheaders', 'rest_framework', 'network', 'rest_framework.authtoken']
 
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
 }
 
-SPECTACULAR_SETTINGS = {
-    'TITLE': 'Django backend API',
-    'DESCRIPTION': 'API for Django backend serving the neural network graph manager',
-    'VERSION': '1.0.0',
-}
 
 LOGGING = {
     "version": 1,
